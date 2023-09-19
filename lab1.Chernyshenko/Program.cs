b625bffbb8776357d9447360e5b38cbfa38d7215
5090969 <5090969@LAPTOP-NB62C61V>
2023-09-19 14:53:30 +0200

    Добавьте файлы проекта.

---
+++ lab1.Chernyshenko/Program.cs
@@ -0,0 +1,29 @@
+﻿//высокий уровень
+//Console.Write("Введите a:");
+//double a = double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите b:");
+//double b = double.Parse(Console.ReadLine()); 
+//Console.WriteLine("Введите x:");
+//double x = double.Parse(Console.ReadLine());
+//Console.Write("Введите z:");
+//double z = double.Parse(Console.ReadLine());
+//double f = (Math.Cos(7) * b * Math.Pow(x, 5) - (Math.Sin(a * a) + Math.Cos((x * x * x) + Math.Pow(z, 5) - (a * a)))) / (Math.Asin(a * a) + Math.Acos(Math.Pow(x, 7) - (a * a)));
+//Console.WriteLine($"f={f:F2}");
+
+//Console.WriteLine("Введите a:"); 
+//double a = double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите b:");
+//double b = double.Parse(Console.ReadLine());
+//Console.WriteLine("Введите h:");
+//double h = double.Parse(Console.ReadLine());
+//double s1 = a * a; 
+//double s2 = b * b;
+//double v = h * (s1 + Math.Sqrt(s1 * s2) + s2) / 3; Console.WriteLine("Объем усеченной пирамиды:" + v);
+
+Console.WriteLine("Введите n:");
+int n = int.Parse(Console.ReadLine()); 
+int m = (n-1) / 4 + 1;
+Console.WriteLine("Место с номером" + n + "В купе номер" + m);
+
+
+
